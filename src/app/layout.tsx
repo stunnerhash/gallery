6941e03cbc78a0591ccfef14f5ac2ca18bc2bcbe
utyadav07860 <utyadav07860@outlook.com>
2024-04-17 02:16:21 +0530

    Clerk Auth done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import "~/styles/globals.css";
 
 import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import TopNav from "./_components/topnav";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -9,18 +11,9 @@ const inter = Inter({
 
 export const metadata = {
   title: "Gallery",
-  description: "gallery",
+  description: "An Awesome Image Galleria",
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
-
-function TopNav(){
-  return ( 
-    <nav className="flex items-center justify-between w-full p-4 text-2xl font-semibold">
-      <div>Gallery</div>
-      <div>Sign In</div>
-    </nav>
-  )
-}
 
 export default function RootLayout({
   children,
@@ -28,11 +21,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={`font-sans ${inter.variable}`}>
-        <TopNav/>
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`font-sans ${inter.variable}`}>
+          <TopNav />
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
