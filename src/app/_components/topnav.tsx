d2b86cd0de8879d506044e73874d6c585f8eaa7d
utyadav07860 <utyadav07860@outlook.com>
2024-04-17 23:43:06 +0530

    cleanup on modal

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { UploadButton } from "~/utils/uploadthing";
 
@@ -8,7 +9,9 @@ export default function TopNav() {
 
   return (
     <nav className="flex w-full items-center justify-between p-4 text-xl font-bold">
-      <div>GALLERY</div>
+      <Link href="/">
+        <div>GALLERY</div>
+      </Link>
 
       <div className="flex">
         <SignedOut>
--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -2,5 +2,14 @@ import { getImage } from "~/server/queries";
 
 export default async function FullPageImageView(props: { id: number }) {
   const image = await getImage(props.id);
-  return <img src={image.url} className="w-96" alt="" />;
+  return (
+    <div className="flex h-full w-full">
+      <div className="flex flex-shrink items-center justify-center">
+        <img src={image.url} className="flex-shrink object-contain" />
+      </div>
+      <div className="flex w-48 flex-shrink-0 flex-col">
+        <div className="text-xl font-bold">{image.name}</div>
+      </div>
+    </div>
+  );
 }
